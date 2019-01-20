4371582c151cdbf99c3e341872fd2ca0825bf34d
francescocretti <francesco.cretti@gmail.com>
2019-01-20 22:12:05 +0100

    add back arrow button to header

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-// $(function() {
-//   var path = window.location.pathname;
-//   var section = window.location.hash.slice(1);
-//   if (path === '/index.html' && section === 'work') {
-//     $('.container').css('display', 'none');
-//     $('#container-work' ).css('display', 'flex');
-//   } else if (path === '/index.html' && section === 'contacts') {
-//     $('.container').css('display', 'none');
-//     $('#container-contacts' ).css('display', 'flex');
-//   }
-// });
+$(function() {
+  var path = window.location.pathname;
+  var section = window.location.hash.slice(1);
+  if (path === '/' && section === 'work') {
+    $('.container').css('display', 'none');
+    $('#container-work' ).css('display', 'flex');
+    $('.menu-item').removeClass('menu-active');
+    $('#work').addClass('menu-active');
+  } else if (path === '/index.html' && section === 'contacts') {
+    $('.container').css('display', 'none');
+    $('#container-contacts' ).css('display', 'flex');
+    $('.menu-item').removeClass('menu-active');
+    $('#contacts').addClass('menu-active');
+  } else {
+    $('.menu-item').removeClass('menu-active');
+    $('#about').addClass('menu-active');
+  }
+  // $(window).scrollTop(0);
+});
 
 $(':not(.menu-separator).menu-item ').click(function() {
   $('.menu-item').removeClass('menu-active');
@@ -16,4 +24,8 @@ $(':not(.menu-separator).menu-item ').click(function() {
   var $container = $('#container-' + $(this).attr('id'));
   $('.container').css('display', 'none');
   $container.css('display', 'flex');
+});
+
+$('#logo').click(function() {
+  window.location = '/';
 });
